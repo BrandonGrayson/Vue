1538dd0f3fa417f09234056dee3ed691697ef360
Brandon <brandonbgrayson@gmail.com>
2021-02-02 20:14:17 -0500

    added form input from react docs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Input from './components/Input';
+
 
 function App() {
 	return (
 		<div className="header-el">
 			<Header />
+      <Input />
 		</div>
 	);
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Input = props => {
     return (
-        <div className="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">@</span>
-            <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"> </input>
-        </div>
+        <form className='mt-5' id='input'>
+            <label>
+                <input type="text" placeholder='Search' name="name" />
+            </label>
+        </form>
     )
 }
+
 
 export default Input
