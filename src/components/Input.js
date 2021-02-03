425d0b812e229eeada1eacc4170d0c3766ed81f6
Brandon <brandonbgrayson@gmail.com>
2021-02-02 20:15:08 -0500

    centered input

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = props => {
     return (
-        <form className='mt-5' id='input'>
+        <form className='mt-5 d-flex justify-content-center' id='input'>
             <label>
                 <input type="text" placeholder='Search' name="name" />
             </label>
