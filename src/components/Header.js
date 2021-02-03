733325be22a05fcc39954f596507960aa0e522c2
Brandon <brandonbgrayson@gmail.com>
2021-02-02 19:10:27 -0500

    added background color/ txt color to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 
 const Header = props => {
     return (
-        <div className='container-fluid'>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Employee Directory</h5>
-                    <p className="card-text">Click on carrots to filter by heading or use the search box to naorrow your results</p>
+        <div className='container-fluid' id='header-card'>
+            <div className="card text-center">
+                <div className="card-body" id='header'>
+                    <h5 className="card-title" id='header-title'>Employee Directory</h5>
+                    <p className="card-text mb-3">Click on carrots to filter by heading or use the search box to naorrow your results</p>
                 </div>
             </div>
         </div>
