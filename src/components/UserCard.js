0136d645e65e3302f1e6f3edf6ea591fa2484c9d
Brandon <brandonbgrayson@gmail.com>
2021-02-04 19:01:37 -0500

    key passed to each table row successfully

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -10,21 +10,22 @@ const UserCard = (props) => {
          props.users.map((el, index) => {
             console.log("This should be the USERS--->", props.users)
             console.log("This is an Mapped El--->", el)
+            console.log("This is the INDEX--->", index)
             // destructure components needed
             const {street, city, state, country} = el.location
             // console.log(el.location)
             const {email} = el.email
             // console.log("This the Email-->", el.email)
-            const { name } = el.name
+            const { first } = el.name.first
             // console.log("This the name--->", el.name)
             const { phone } = el.phone
             // console.log('This is the phone number--->', el.phone)
             const { thumbnail } = el.picture
             // console.log("This is image thumbnail--->", el.picture)   
-            trs.push(<tr>
-                    <td key={index}> 
+            trs.push(<tr key={index}>
+                    <td> 
                         <img src={thumbnail}/> </td>
-                        <td>{name}</td>
+                        {/* <td>{name}</td> */}
                     </tr>
          );
         });
